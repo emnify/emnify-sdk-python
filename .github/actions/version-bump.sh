4fd6a43fe6c7010341faf7c81b96aa894766c585
Mikhail G <mg@rademade.com>
2022-06-07 15:53:36 +0300

    WIP export versions

--- .github/actions/version-bump.sh
+++ .github/actions/version-bump.sh
@@ -1,4 +1,7 @@
 bump2version $1
 
-echo '::set-output name=previous_version::$BUMPVERSION_CURRENT_VERSION'
-echo '::set-output name=current_version::$BUMPVERSION_NEW_VERSION'
+echo $BUMPVERSION_CURRENT_VERSION
+echo $BUMPVERSION_NEW_VERSION
+
+echo "::set-output name=previous_version::$BUMPVERSION_CURRENT_VERSION"
+echo "::set-output name=current_version::$BUMPVERSION_NEW_VERSION"
