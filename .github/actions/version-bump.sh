c3cbfb2829b6814c2521cd6c7ba1c34f95f17df8
Mikhail G <mg@rademade.com>
2022-06-07 15:57:44 +0300

    Removed docker for CI

--- .github/actions/version-bump.sh
+++
@@ -1,7 +0,0 @@
-bump2version $1
-
-echo $BUMPVERSION_CURRENT_VERSION
-echo $BUMPVERSION_NEW_VERSION
-
-echo "::set-output name=previous_version::$BUMPVERSION_CURRENT_VERSION"
-echo "::set-output name=current_version::$BUMPVERSION_NEW_VERSION"
