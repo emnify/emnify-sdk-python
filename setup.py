725d091f2326ad13f8259641a1d8f27245c61325
ArturLazarenko <ArturLazarenko@users.noreply.github.com>
2022-06-09 10:23:27 +0000

    Automated version bump from 0.0.3 to 0.0.4.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 NAME = "emnify-python-sdk"
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 # To install the library, run the following
 #
 # python setup.py install
