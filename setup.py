d554e40e54b762ea8b10c5a478226f298e8252af
Mikhail G <mg@rademade.com>
2022-06-09 17:01:24 +0300

    chore(ci): migrate to alpine image

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ VERSION = "0.0.7"
 
 REQUIRES = []
 
-with open("Readme.md", "r", encoding="utf-8") as fh:
+with open('README.md', "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
