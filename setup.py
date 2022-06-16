e70799a5c938a45a6d22196a74fc9d5236e34f67
ArturLazarenko <ArturLazarenko@users.noreply.github.com>
2022-06-16 12:30:46 +0000

    Automated version bump from 0.0.5 to 0.0.7.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import os
 
 NAME = os.getenv('PYPI_PACKAGE_NAME') or "emnify-python-sdk"
-VERSION = "0.0.5"
+VERSION = "0.0.7"
 # To install the library, run the following
 #
 # python setup.py install
