2317cd972efc0a9f2a6b2066ec643cb715b77545
Carolyn Stransky <hello@workwithcarolyn.com>
2023-08-04 13:24:18 +0200

    docs: edit example comments to match documentation style guide

--- docs/examples/mass_sim_activation.py
+++ docs/examples/mass_sim_activation.py
@@ -2,52 +2,50 @@ from emnify import EMnify
 from emnify import constants
 from emnify.errors import EMnifyBaseException
 
-# === Example: Getting First Devices Online ===
+# === Example: Get your first device online ===
 
-# To operate EMnify SDK you need to generate application token.
-# That is how: https://www.emnify.com/developer-blog/how-to-use-an-application-token-for-api-authentication
+# To operate the emnify SDK, you need to generate an application token.
+# Step-by-step guide: https://www.emnify.com/developer-blog/how-to-use-an-application-token-for-api-authentication
 token = input('token: ')
-# The client is authorized to perform operations by your name;
+# Authorize the client to perform operations:
 emnify_client = EMnify(token)
 
-# There are two things you need to obtain before you get you device online,
-# one is a device and other is a SIM card.
-# For this example we assume you have a batch of SIM cards you willing to
-# use in your devices.
+# Before going online, you need a device and a SIM card.
+# This example assumes you have a batch of SIM cards for your devices.
 sim_batch_BIC2 = input('BIC2: ')
 
-# EMnify allows you to control coverage and services you need.
-# Lookup Concepts to learn more.
-# https://portal.emnify.com/device-policies you can find IDs there.
+# emnify allows you to control your service and coverage policies.
+# You can find those IDs on the Portal: https://portal.emnify.com/device-policies 
 service_profile_id = input('Service Profile ID: ')
 tariff_profile_id = input('Tariff Profile ID: ')
-# In order to create devices for SIMs afterwards we need service and coverage profiles.
+# Then, you need service and coverage profiles to create devices later:
 service_profile = emnify_client.devices.service_profile_model(id=int(service_profile_id))
 tariff_profile = emnify_client.devices.tariff_profile_model(id=int(tariff_profile_id))
 
 try:
-    # Having a batch of SIM cards you need to add them to your account;
-    # This method also supports single sim registration via BIC1,
-    # so you can use your FREE Evaluation SIM cards either;
+    # Next, add the SIM card batch to your account.
+    # This method also supports single SIM registration via BIC1
+    # so you can use your free Evaluation SIM cards.
     issued_sims = emnify_client.sim.register_sim(bic=sim_batch_BIC2)
-    # After it's added - all sim`s are registred with "issued" status;
+    # All added SIMs are now registered with "Issued" status.
 except EMnifyBaseException as e:
-    # It can happen that there is some error appear during SIM registration;
-    # Use EMnifyBaseException for general exceptions or inherited classes for specific one`s;
+    # If an error appears during SIM registration,
+    # use EMnifyBaseException for general exceptions 
+    # or inherited classes for specific ones.
     raise AssertionError(f"error during sim batch BIC2 activation{e}")
 
-# We define status of device to be applied during creation.
+# Define the device status to apply when creating the device.
+# In this example, the device status would be "Enabled"
 device_status = emnify_client.devices.status_model(
     **constants.DeviceStatuses.ENABLED_DICT.value
 )
 
 
 for sim in issued_sims:
-    # SIM cards do not provide connectivity by just registering them,
-    # So in order to get devices you put SIM cards in you also need to
-    # create a new device with the SIM assigned.
+    # Only registering a SIM card won't provide connectivity.
+    # You also need to create a new device with the SIM assigned.
 
-    # For device creation we need to specify parameters of the device:
+    # To do this, specify the parameters of the device:
     device_name = f"Device({sim.iccid})"
     device_model = emnify_client.devices.device_create_model(
         tariff_profile=tariff_profile,
@@ -56,29 +54,30 @@ for sim in issued_sims:
         sim=sim,
         name=device_name
     )
-    # See API Reference to learn other parameters the device can have.
+    # See the API Reference to learn other device parameters:
+    # https://emnify.github.io/emnify-sdk-python/autoapi/index.html
 
-    # And there is how we create a device we wanted.
+    # Then, create the device:
     device_id = emnify_client.devices.create_device(device=device_model)
 
-    # After creation we may retrieve full device information.
-    # Which you can store in your local inventory for future needs.
+    # Once created, you can retrieve the device information.
+    # You can store this information in your local inventory for future needs.
     device = emnify_client.devices.retrieve_device(device_id=device_id)
 
-    # As it mentioned above by default connectivity is disabled so you're not getting billed.
-    # The following command will enable connectivity for your device with a SIM card installed.
+    # Connectivity is turned off by default (so you aren't billed).
+    # The following command enables connectivity for your device:
     emnify_client.devices.change_status(enable=True, device=device)
-    # At this point EMnify is ready to provide you connectivity services.
-    # So we can already send and receive SMS(if this enabled in the Service Profile assigned).
 
-    # After making sure device is created and enabled
-    # it is time to perform configuration on a device side.
+    # At this point, emnify can provide connectivity services.
+    # You can send and receive SMS (if enabled in the assigned Service Profile).
 
-    # Correct APN configuration of the device is required to access internet.
-    # EMnify APN is(no spaces, just two characters): em
-    # For example purposes we will send a special configuration SMS command.
-    # It may vary by the device manufacturer, see our documentation to learn
-    # if this method suites your devices.
+    # To access the internet, configure the APN for your device.
+
+    # The emnify APN is: em (two characters, no spaces)
+    # This configuration may vary by the device manufacturer.
+    # See the emnify Documentation: https://docs.emnify.com/apn-configuration
+
+    # The following example sends a special configuration SMS command:
     ACTIVATION_CODE = 'AT+CGDCONT=1,"IP","em",,'
     SENDER = "city_scooters_admin"
 
@@ -87,8 +86,8 @@ for sim in issued_sims:
         source_adress=SENDER
     )
 
-    # And now we send the configuration SMS to our device.
+    # Finally, send the configuration SMS to your device:
     emnify_client.devices.send_sms(device=device, sms=activation_sms)
 
-    # CONGRATULATIONS! You get your device online!
-    # Now you can check your device internet access.
+    # Congratulations! Your device is online.
+    # If you run into problems using this example, please open an issue.
