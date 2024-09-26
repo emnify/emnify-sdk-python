4274734e9b1a823132a205c82ad063d0f6133a9a
Th3Un1q3 <Th3Un1q3@users.noreply.github.com>
2024-09-26 13:50:07 +0000

    [Automated] Version Bump from 0.3.9 to 0.3.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import os
 
 NAME = os.getenv('PYPI_PACKAGE_NAME') or "emnify-sdk"
-VERSION = "0.3.9"
+VERSION = "0.3.10"
 # To install the library, run the following
 #
 # python setup.py install
